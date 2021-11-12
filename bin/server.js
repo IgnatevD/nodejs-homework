a56189b406c225cd01189978b7d28cf46655f972
IgnatevD <ignatevds@gmail.com>
2021-11-12 20:04:16 +0200

    обновление ДЗ4

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,27 @@
 /** @format */
-
 const app = require("../app");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
+const DB_URL = process.env.DB_URL;
+
+const dataBaseNew = async () => {
+  try {
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection successful");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+dataBaseNew();
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
